a39898ff8b2620fdd34e82f78ccec7cdd83a6e99
RyogaGrey <sergryvolk86@gmail.com>
2024-07-01 11:22:40 +0500

    Попытался настроить docker, prometheus и alertmanager

---
+++ src/i18n.ts
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const resources = {
+  en: {
+    translation: {
+      welcome: 'Welcome to the Profile page',
+    },
+  },
+  ru: {
+    translation: {
+      welcome: 'Добро пожаловать на страницу профиля',
+    },
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/index';
 import reportWebVitals from './reportWebVitals';
+import './i18n'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
